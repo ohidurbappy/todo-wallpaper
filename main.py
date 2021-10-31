658e4ff2852bf0f80eb1e73742fb9668cbe7213a
Ohidur Rahman Bappy <ohidurbappy@gmail.com>
2021-11-01 01:39:47 +0600

    option to customize font,size,bg

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import os
 import ctypes
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont,ImageOps
 import time
-
-
-APP_PATH=os.path.abspath(os.path.dirname(__file__))
-FONT_FILENAME="OpenSans-Regular.ttf"
-BACKGROUND_FILENAME="1.png"
-TODO_FILENAME="todo.txt"
-OUTPUT_DIR=os.path.join(APP_PATH,'output')
-OUTPUT_IMAGE_PATH=os.path.join(APP_PATH,'output','background.png')
+import sys
 
 
 def set_wallpaper(image_path):
@@ -22,21 +15,105 @@ def strikethrough(text):
         result = result + c + '\u0336'
     return result
 
-todos=[line.strip() for line in open(os.path.join(APP_PATH,TODO_FILENAME),encoding='utf-8')]
+def get_dominant_color(pil_img):
+    img = pil_img.copy()
+    #img=ImageOps.invert(img)
+    img.convert("RGB")
+    img.resize((1, 1), resample=0)
+    dominant_color = img.getpixel((0, 0))
+    return dominant_color
 
-image=Image.open(os.path.join(APP_PATH,"background",BACKGROUND_FILENAME))
-fnt=ImageFont.truetype(os.path.join(APP_PATH,'fonts',FONT_FILENAME),32)
 
+def get_screen_size():
+    cmd = "wmic path Win32_VideoController get CurrentVerticalResolution,CurrentHorizontalResolution"
+    (x,y) = tuple(map(int,os.popen(cmd).read().split()[-2::]))
+    return (x,y)
+
+
+APP_PATH=os.path.abspath(os.path.dirname(__file__))
+TODO_FILENAME="todo.txt"
+OUTPUT_DIR=os.path.join(APP_PATH,'output')
+OUTPUT_IMAGE_PATH=os.path.join(APP_PATH,'output','background.png')
+
+# default configurations
+c_font_name="OpenSans-Regular.ttf"
+c_font_name="lucida-console.ttf"
+c_background="img19.jpg"
+c_title="Todo"
+c_font_size=22
+
+thismodule = sys.modules[__name__]
+
+def process_instruction(instruction:str):
+    instruction=instruction.strip("[")  
+    instruction=instruction.strip("]")
+    try:  
+        var,val=instruction.split("=")
+        c_var="c_"+var
+        if c_var in globals():
+            # setattr(thismodule,"c_"+var,val)
+            if val.isnumeric():
+                val=int(val)
+            globals()[c_var]=val
+
+    except ValueError:
+        pass
+    
+    
+        
+    
+
+
+lines=[line for line in open(os.path.join(APP_PATH,TODO_FILENAME),encoding='utf-8')]
+
+todos=[]
+
+# read the configuration
+for line in lines:
+    pre_line=line.strip()
+    if pre_line.startswith("[") and pre_line.endswith("]"):
+        process_instruction(pre_line)
+    else:
+        todos.append(line)
+
+
+
+
+image=Image.open(os.path.join(APP_PATH,"background",c_background))
+
+basewidth = get_screen_size()[0]
+wpercent = (basewidth/float(image.size[0]))
+hsize = int((float(image.size[1])*float(wpercent)))
+image = image.resize((basewidth,hsize), Image.ANTIALIAS)
+
+
+
+fnt=ImageFont.truetype(os.path.join(APP_PATH,'fonts',c_font_name),c_font_size)
 d1=ImageDraw.Draw(image)
 
 width,height=image.size
 
-w=width-round(width/4)
-h=round(height/5)
+x=width-round(width/4)*1
+y=round(height/5)
+
+w, h = fnt.getsize(c_title)
+w+=10
+h+=2
+
+lCenter=(((width-x)-w/2)/6)*1
+
+
+# (2,108,248)
+d1.rectangle((x+lCenter, y, x + w+lCenter, y + h), fill=get_dominant_color(image))
+
+
+
+d1.text((x+5+lCenter, y), c_title, font=fnt, fill =(255, 255, 255,255))
+y+=c_font_size+2
 
 for todo_item in todos:
-    d1.text((w, h), todo_item, font=fnt, fill =(255, 255, 255))
-    h+=34
+    d1.text((x, y), todo_item, font=fnt, fill =(255, 255, 255))
+    y+=c_font_size+2
 
 
 if not os.path.exists(OUTPUT_DIR):
